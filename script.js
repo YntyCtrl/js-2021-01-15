e37f123c6f512161516c6681fd10877b31d78788
YntyCtrl <yntymak.199848@gmai.com>
2021-01-15 15:56:03 +0600

    end all

---
+++ script.js
@@ -0,0 +1,66 @@
+let first = "yntymak";
+first = "shrek";
+let secend = first;
+let third = secend;
+let fourth = third;
+
+
+
+let fifth = fourth;
+
+console.log(fifth);
+
+let capital = "Bishkek"
+let country = "KG"
+console.log(capital + " is the capital of " + country);
+
+let unit = "meters";
+let subject = "distans";
+
+unit = "volume";
+subject = "liters";
+
+
+console.log(unit + " is measuted in " + subject);
+
+let tytle = "Dark Tour";
+let author = "SB";
+
+
+console.log("My favourite book is " + tytle + "by" + author);
+
+console.log("my favourite song is \"Yesterdy\" by Beatles");
+console.log('my favourite song is "Yesterdy" by Beatles');
+console.log('i\'m the best!');
+
+
+let artist = "Beatles"
+console.log("I'm the far of " + "\"" + artist + "\"" + "!");
+console.log(`I'm the fan of "` + artist + `"!`);
+console.log(`I'm the fan of "${artist}"!`);
+
+let kino = "Terminanor"
+console.log(`My fovourite movie is "${kino}" `);
+
+// let firstName = prompt('What is your name?');
+
+// console.log(`Your name is ${firstName}`);
+
+
+// let a = +prompt("what is A");
+// let b = +prompt("what is B");
+
+// let sum = a + b ;
+
+
+
+// console.log(`The sum of two numbers is equal to ${sum}`);
+
+let c = +prompt("what is C");
+let d = +prompt("what is D");
+
+let result = c * d ;
+
+
+
+console.log(`C multiplied by D is equal to ${result}`);
